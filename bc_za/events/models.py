06102838e164f744ca03877259fc34e74645ec4b
Karine Osipova <osipovakarine@gmail.com>
2023-11-28 21:20:01 +0100

    add initial event model

--- bc_za/events/models.py
+++ bc_za/events/models.py
@@ -1,3 +1,18 @@
+"""Models for the Events app."""
+
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    """Event model class."""
+
+    name = models.CharField(max_length=255)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    location = models.CharField(max_length=255)
+    max_number_participants = models.IntegerField(verbose_name="maximum number of participants")
+    category = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        """Represent the event as a string."""
+        return str(self.name)
