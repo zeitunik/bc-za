b675626dd9ad8516512d0e77a8060335c7eea49f
DropD <r.haeuselmann@gmx.ch>
2023-11-28 21:40:59 +0100

    switch on login page and setup templates folder

--- bc_za/bc_za/urls.py
+++ bc_za/bc_za/urls.py
@@ -15,8 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("login/", auth_views.LoginView.as_view())]
