4d5d354ee8891a43230d4b464aaef39d4ed6dd70
zeitunik <osipovakarine@gmail.com>
2024-08-01 22:08:36 +0200

    add detail page url

--- bc_za/events/urls.py
+++ bc_za/events/urls.py
@@ -3,4 +3,4 @@ from django.urls import path
 from . import views
 
 
-urlpatterns = [path("", views.index, name="events")]
+urlpatterns = [path("", views.index, name="events"), path("<int:event_id>/", views.detail, name="detail")]
