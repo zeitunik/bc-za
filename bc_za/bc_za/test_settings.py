3076204aa0a4825b5b8271856b89f58f2ff81c3e
zeitunik <osipovakarine@gmail.com>
2024-08-01 11:00:24 +0200

    switch to ruff

--- bc_za/bc_za/test_settings.py
+++ bc_za/bc_za/test_settings.py
@@ -1,7 +1,9 @@
 """Override some production settings for tests."""
+
 import os
 
 from .settings import *  # noqa: F403  # we really want to import everything
+
 
 DATABASES["default"]["NAME"] = os.environ.get(  # noqa: F405  # overriding settings on purpose
     "BCZA_TEST_DB", "db.tests"
