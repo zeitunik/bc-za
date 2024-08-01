a74b05f6bf69b608c31c15ac365ea26a8c4dbb1b
zeitunik <osipovakarine@gmail.com>
2024-08-01 22:08:16 +0200

    add template to index, add detail view

--- bc_za/events/views.py
+++ bc_za/events/views.py
@@ -1,6 +1,14 @@
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+
+from .models import Event
 
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Nothing to see here, try logging in.")
+    upcoming_events = Event.objects.order_by("start")[:5]
+    context = {"upcoming_events": upcoming_events}
+    return render(request, "events/index.html", context)
+
+
+def detail(request, event_id):
+    event = get_object_or_404(Event, pk=event_id)
+    return render(request, "events/detail.html", {"event": event})
